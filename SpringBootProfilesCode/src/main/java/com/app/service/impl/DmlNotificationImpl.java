11e185ef4f6ed9205952723da4fa58493cfb826f
Amit <Amit@192.168.0.40>
2019-06-09 23:36:59 +0530

    initial stage

---
+++ SpringBootProfilesCode/src/main/java/com/app/service/impl/DmlNotificationImpl.java
@@ -0,0 +1,17 @@
+package com.app.service.impl;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import com.app.service.INotificationService;
+@Component
+@Profile("dml")
+public class DmlNotificationImpl implements INotificationService {
+
+	@Override
+	public void notification() {
+		
+		System.out.println("from dml only");
+	}
+
+}
